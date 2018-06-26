d52022c6a444644d409dc1126a4fc4870e039c33
kpose <ganihujude@gmail.com>
2018-06-26 23:02:57 +0100

    Show notes in a cardview and recycler view.

--- app/src/main/java/com/example/kpose/journal/MainActivity.java
+++ app/src/main/java/com/example/kpose/journal/MainActivity.java
@@ -1,26 +1,90 @@
 package com.example.kpose.journal;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private RecyclerView mNotesList;
+    private GridLayoutManager gridLayoutManager;
+
+    private DatabaseReference fNotesDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        mNotesList = findViewById(R.id.main_notes_list);
+
+        gridLayoutManager = new GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false);
+
+        mNotesList.setHasFixedSize(true);
+        mNotesList.setLayoutManager(gridLayoutManager);
+
         firebaseAuth = FirebaseAuth.getInstance();
 
+        if (firebaseAuth.getCurrentUser() != null) {
+            fNotesDatabase = FirebaseDatabase.getInstance().getReference().child("Notes").child(firebaseAuth.getCurrentUser().getUid());
+        }
+
         updateUI();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<NoteModel, NoteViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<NoteModel, NoteViewHolder>(
+
+
+                NoteModel.class,
+                R.layout.single_note_layout,
+                NoteViewHolder.class,
+                fNotesDatabase
+        ) {
+            @Override
+            protected void populateViewHolder(final NoteViewHolder viewHolder, NoteModel model, int position) {
+
+                String noteId = getRef(position).getKey();
+
+                fNotesDatabase.child(noteId).addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String title = dataSnapshot.child("title").getValue().toString();
+                        String timestamp = dataSnapshot.child("timestamp").getValue().toString();
+
+                        viewHolder.setNoteTitle(title);
+                        viewHolder.setNoteTime(timestamp);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        };
+
+        mNotesList.setAdapter(firebaseRecyclerAdapter);
     }
 
     private void updateUI() {
