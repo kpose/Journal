8dbc9be6db3f76a588d672e4653728053611a9c4
kpose <ganihujude@gmail.com>
2018-06-26 17:31:01 +0100

    Created a model to save our notes

---
+++ app/src/main/java/com/example/kpose/journal/NoteModel.java
@@ -0,0 +1,32 @@
+package com.example.kpose.journal;
+
+public class NoteModel {
+
+    public String noteTitle;
+    public String noteTime;
+
+    public NoteModel() {
+
+    }
+
+    public NoteModel(String noteTitle, String noteTime) {
+        this.noteTitle = noteTitle;
+        this.noteTime = noteTime;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public String getNoteTime() {
+        return noteTime;
+    }
+
+    public void setNoteTime(String noteTime) {
+        this.noteTime = noteTime;
+    }
+}
